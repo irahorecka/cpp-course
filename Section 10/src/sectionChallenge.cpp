5949338fc6fddb9e500b72634c8db33b73679fa5
Ira Horecka <ira89@icloud.com>
2020-07-20 12:30:42 -0700

    complete Section 10, assignment 1

--- Section 10/src/sectionChallenge.cpp
+++ Section 10/src/sectionChallenge.cpp
@@ -1,67 +1,53 @@
-// Section 10
-// Challenge - 
-// Substitution Cipher
-
-/*
-A simple and very old method of sending secret messages is the substitution cipher.
-You might have used this cipher with your friends when you were a kid.
-Basically, each letter of the alphabet gets replaced by another letter of the alphabet.
-For example, every 'a' get replaced with an 'X', and every 'b' gets replaced with a 'Z', etc.
-
-Write a program thats ask a user to enter a secret message.
-
-Encrypt this message using the substitution cipher and display the encrypted message.
-Then decryped the encrypted message back to the original message.
-
-You may use the 2 strings below for  your subsitition.
-For example, to encrypt you can replace the character at position n in alphabet 
-with the character at position n in key.
-
-To decrypt you can replace the character at position n in key
-with the character at position n in alphabet.
-
-Have fun! And make the cipher stronger if you wish!
-Currently the cipher only substitutes letters - you could easily add digits, puncuation, whitespace and so
-forth. Also, currently the cipher always substitues a lowercase letter with an uppercase letter and vice-versa.
-This could also be improved.
-
-Remember, the less code you write the less code you have to test!
-Reuse existing functionality in libraries and in the std::string class!
-*/
-
 #include <iostream>
+#include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
+
 int main() {
-    
-    string alphabet {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-    string key  {"XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr"};
-    // let's work with encryption i.e. alphabet to string
+    // Build ascii vector for pyramid challenge
+    // Actually not necessary, haha
+    // vector<char> ascii {};
+    // vector<int> omit_ascii {91, 92, 93, 94, 95, 96};  // ascii ints to omit
 
-    cout << "Input a word: ";
-    string userInput;
-    getline(cin, userInput);  // get up to the first space, reject rest
+    // for (int(i) {65}; i <= 122; i++) {
+    //     if (find(omit_ascii.begin(), omit_ascii.end(), i) != omit_ascii.end()) {
+    //         ;
+    //     } else {
+    //         ascii.push_back((char)i);
+    //         // cout << (char)i;
+    //     }
+    // }
 
-    cout << endl;
-    string encrypInput;
-    for (char c: userInput) {
-        encrypInput += key.at(alphabet.find(c));
+    string user_input {};
+    cout << "Choose a string of char or int: ";
+    cin >> user_input;
+
+    for (int i{0}; i < user_input.length(); i++) {
+        if (i > 0) {
+            string forward {user_input.substr(0, i+1)};
+            string backward {user_input.substr(0, i)};
+            reverse(backward.begin(), backward.end());
+
+            auto spaces_num {static_cast<int>(((user_input.length()*2 -1) - (forward.length() + backward.length())) / 2)};
+            for (int j{0}; j < spaces_num; j++) {
+                cout << " ";
+            }
+
+            cout << forward << backward << endl;
+
+        } else {
+            // first index of user_input string
+            auto spaces_num_1 {static_cast<int>((user_input.length()*2 -1) - 1) / 2};
+            for (int j{0}; j < spaces_num_1; j++) {
+                cout << " ";
+            }
+
+            cout << user_input.at(i) << endl;
+        }
     }
-    cout << "Encrypted message: " <<  encrypInput << endl;
 
-    string decryptMessage;
-    for (char c: encrypInput) {
-        decryptMessage += alphabet.at(key.find(c));
-    }
-    cout << "Decrypted message: " <<  decryptMessage << endl;
-
-
-
-
-    
-    cout << endl;
     return 0;
 }
-
